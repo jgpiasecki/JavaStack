d8d10106719c008d5ceb3d8e2b9444d689442f45
John Piasecki <jgpie189@outlook.com>
2017-10-26 14:36:11 -0400

    added array list empty pop and push

---
+++ MyStack/src/mystack/newStack.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mystack;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jpiasecki
+ */
+public class newStack<T> {
+    
+    private ArrayList <T> myList; 
+    
+    public newStack() 
+    {
+     myList = new ArrayList<>();
+    }
+    
+    public void isEmpty()
+    {
+         System.out.println("The stack is empty");
+    }
+    
+    public T pop()
+    {
+        T last;
+        last = myList.remove((myList.size()-1));
+        return last;
+    }
+    
+    public void push(T x)
+    {
+        myList.add(x);
+    }
+    
+}
