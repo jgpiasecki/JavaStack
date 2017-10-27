edd47af1466b3109266c2adcea069d5a28130bf8
John Piasecki <jgpie189@outlook.com>
2017-10-27 14:27:24 -0400

    added things

--- MyStack/src/mystack/MyStack.java
+++ MyStack/src/mystack/MyStack.java
@@ -17,18 +17,18 @@ public class MyStack {
      */
     public static void main(String[] args) {
         
-      newStack myStack = new newStack();
+      newStack myStack = new newStack();//calls for the stack
        
-      ArrayList <String> John = new ArrayList<>();
+      ArrayList <String> John = new ArrayList<>();//creats a list of strings 
       
-     John.add("Ethan");
+     John.add("Ethan");//adds items to your list
      John.add("Israel");
      John.add("Parker");
      
-     for(int i=2; i<=0; i--)
+     for(int i=2; i>=0; i--)
      {
-         myStack.push(John.get(i));
-         System.out.println(myStack.pop());
+         myStack.push(John.get(i));//adds the list to the stack
+         System.out.println(myStack.pop());//prints the list and pops
      }
      
      
